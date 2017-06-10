63433fff10d4945f8bd9a459032e49b52437217c
elzup <guild0105@gmail.com>
2017-06-10 15:39:22 +0900

    Empty logic fix

--- app/reducers/board.js
+++ app/reducers/board.js
@@ -62,7 +62,7 @@ export default function counter(state: boardStateType = initState, action: actio
               return;
             }
             const tSquare = state.squares[ty][tx];
-            const isEmpty = tSquare === 'empty';
+            const isEmpty = tSquare.owner === 'empty';
             const isMine = tSquare.owner === hand;
             const isEnemy = !isMine && !isEmpty;
             if (isEmpty) {
@@ -95,45 +95,46 @@ export default function counter(state: boardStateType = initState, action: actio
 }
 
 function enableCheck(oldSquares: Array<Array<Square>>, hand: PlayerType): Array<Array<Square>> {
-  const squares = [...oldSquares]; _.each(_.range(squares.length), ny => {
-    _.each(_.range(squares[0].length), nx => {
-      const res = !_.every(
-        _.map([-1, 0, 1], dx =>
-          _.map([-1, 0, 1], dy => {
-            if (dx === 0 && dy === 0) {
+  const squares = [...oldSquares];
+  _.each(_.range(squares.length), y => {
+    _.each(_.range(squares[0].length), x => {
+      if (squares[y][x].owner !== 'empty') {
+        squares[y][x].enable = false;
+        return;
+      }
+      const maps = _.map([-1, 0, 1], dx =>
+        _.map([-1, 0, 1], dy => {
+          if (dx === 0 && dy === 0) {
+            return false;
+          }
+          let i = 1;
+          let existsEnemyStone = false;
+          while (true) {
+            const tx = x + dx * i;
+            const ty = y + dy * i;
+            if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8) {
               return false;
             }
-            let i = 1;
-            let existsEnemyStone = false;
-            while (true) {
-              const tx = nx + dx * i;
-              const ty = ny + dy * i;
-              if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8) {
-                return false;
-              }
-              const tSquare = squares[ty][tx];
-              const isEmpty = tSquare === 'empty';
-              const isMine = tSquare.owner === !hand;
-              const isEnemy = !isMine && !isEmpty;
-              if (isEmpty) {
-                return false;
-              }
-              if (isEnemy) {
-                existsEnemyStone = true;
-              } else {
-                if (existsEnemyStone) {
-                  break;
-                }
-                return false;
-              }
-              i += 1;
+            const tSquare = squares[ty][tx];
+            const isEmpty = tSquare.owner === 'empty';
+            const isMine = tSquare.owner === hand;
+            const isEnemy = !isMine && !isEmpty;
+            if (isEmpty) {
+              return false;
             }
-            return true;
-          })
-        ), t => !t);
-      if (res) {
-        squares[ny][nx].enable = true;
-      }
+            if (isEnemy) {
+              existsEnemyStone = true;
+            } else {
+              if (existsEnemyStone) {
+                break;
+              }
+              return false;
+            }
+            i += 1;
+          }
+          return true;
+        }));
+      squares[y][x].enable = _.some(_.flatten(maps), Boolean);
     });
   });
   return squares;
