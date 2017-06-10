29795ac0958d319baade4a5c8f784e5c2b7f51ae
elzup <guild0105@gmail.com>
2017-06-10 15:18:33 +0900

    Enable style

--- app/components/Othello.js
+++ app/components/Othello.js
@@ -25,6 +25,14 @@ const stoneStyle = {
   empty: {}
 };
 
+const enableStyle = {
+  borderRadius: 5,
+  margin: 5,
+  width: 40,
+  height: 40,
+  border: 'solid yellow 2px'
+};
+
 class Othello extends Component {
   props: {
     board: boardStateType,
@@ -66,7 +74,7 @@ class Othello extends Component {
                         changeHand();
                       }}
                     >
-                      <div style={stoneStyle[square.owner]} />
+                      <div style={{ ...stoneStyle[square.owner], ...(square.enable ? enableStyle : {}) }} />
                     </td>
                   );
                 })}
